5c3d87e286287647efc938c8183f5823173d7769
umbat <farhanpratamax@gmail.com>
2022-06-16 11:39:51 +0700

    success to fill .json but don't know what inside

--- remote/remote convert.py
+++ remote/remote convert.py
@@ -1,5 +1,12 @@
 import pandas as pd
 import json
+import csv
 
-df = pd.read_csv('https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/CSV/kecamatanforecast-banten.csv')
-print(df)
+f = pd.read_csv('https://data.bmkg.go.id/DataMKG/MEWS/DigitalForecast/CSV/kecamatanforecast-banten.csv')
+data = {"forecast": []}
+for row in f:
+    data["forecast"].append({"locationCode": row[0], "time": row[1], "Tmin": row[2], "Tmax": row[3], "Hmin": row[4], "Hmax": row[5], "humidity": row[6], "temperature": row[7], "weather": row[8], "Wdir": row[9], "Wspeed": row[10]})
+print (data)
+
+with open ("remote/banten.json", "w") as f:
+    json.dump(data, f, indent=4)
